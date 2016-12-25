efd5932cc85c9657c18e9298493e16c07271c5d1
Pankaj Saharan <pankaj.saharan@gmail.com>
2016-12-26 01:31:45 +0200

    minor fixes for heroku app

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 // Move all https traffic to http
 app.use(function(req, res, next){
-  if (req.headers['x-forward-proto' === 'http']) {
+  if (req.headers['x-forward-proto'] === 'http') {
     next();
   } else {
     res.redirect('http://' + req.hostname + req.url);
